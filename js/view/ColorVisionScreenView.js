24bf58d330f9ebb5f86bdf4d820b0dc18cdfbf62
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-05-13 14:28:38 -0600

    add flashlights to view

--- js/view/ColorVisionScreenView.js
+++ js/view/ColorVisionScreenView.js
@@ -15,28 +15,61 @@
   var Image = require( 'SCENERY/nodes/Image' );
   var VBox = require( 'SCENERY/nodes/VBox' );
   var ColorVisionSlider = require( 'COLOR_VISION/view/ColorVisionSlider' );
+  var Vector2 = require( 'DOT/Vector2' );
 
   // images
   var mockupImage = require( 'image!COLOR_VISION/mockup1.png' );
+  var flashlight = require( 'image!COLOR_VISION/flashlight.png' );
 
   function ColorVisionScreenView( model ) {
 
     ScreenView.call( this );
 
-    this.addChild( new Image( mockupImage, {
-      centerX: this.layoutBounds.centerX,
-      centerY: this.layoutBounds.centerY,
-      scale: this.layoutBounds.height / mockupImage.height,
-      opacity: 0.5
-    } ) );
+    this.addChild( new Image( mockupImage,
+      {
+        centerX: this.layoutBounds.centerX,
+        centerY: this.layoutBounds.centerY,
+        scale: this.layoutBounds.height / mockupImage.height,
+        opacity: 0.5
+      } ) );
+
+    var flashlightScale = new Vector2( 0.36, 0.2 );
+
+    var redFlashlight = new Image( flashlight,
+      {
+        scale: flashlightScale,
+        rotation: -Math.PI / 6
+      } );
+
+    var greenFlashlight = new Image( flashlight,
+      {
+        scale: flashlightScale,
+      } );
+
+    var blueFlashlight = new Image( flashlight,
+      {
+        scale: flashlightScale,
+        rotation: Math.PI / 6
+      } );
+
+    var flashlightVBox = new VBox(
+      {
+        children: [
+          redFlashlight,
+          greenFlashlight,
+          blueFlashlight ],
+        spacing: 75,
+        x: this.layoutBounds.maxX - 200,
+        centerY: this.layoutBounds.centerY
+      } );
+
+    this.addChild( flashlightVBox );
 
     var redSlider = new ColorVisionSlider( model.redIntensityProperty, 'red' );
     var greenSlider = new ColorVisionSlider( model.greenIntensityProperty, 'green' );
     var blueSlider = new ColorVisionSlider( model.blueIntensityProperty, 'blue' );
 
-    console.log(this.layoutBounds.maxX);
-
-    var vBox = new VBox(
+    var sliderVBox = new VBox(
       {
         children: [
           redSlider,
@@ -47,7 +80,7 @@
         centerY: this.layoutBounds.centerY
       } );
 
-    this.addChild( vBox );
+    this.addChild( sliderVBox );
 
   }
 
