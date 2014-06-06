e27d8945c55e8488b07874c4e53e5e62b7d9cd43
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-06-06 10:58:32 -0600

    change homescreen icon background color

--- js/ColorVisionConstants.js
+++ js/ColorVisionConstants.js
@@ -17,7 +17,7 @@ define( function( require ) {
     SINGLE_BEAM_LENGTH: 280,
     FLASHLIGHT_ANGLE: Math.PI / 6,
     CENTER_Y_OFFSET: -20,
-    HOME_SCREEN_ICON_FILL: '#e7e9cc',
+    HOME_SCREEN_ICON_FILL: '#222222',
     GAUSSIAN_WIDTH: 50, // in units of wavelengths
     X_VELOCITY: -240,  // x-velocity of photons
     FAN_FACTOR: 1.05,   // amount of fanning of photons
