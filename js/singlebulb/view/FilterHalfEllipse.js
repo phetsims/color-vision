099149d85f6fb2cf8d8a4d0559bd50696dfca753
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-06-18 11:21:26 -0600

    simplify inheritence in FilterHalfEllipse

--- js/singlebulb/view/FilterHalfEllipse.js
+++ js/singlebulb/view/FilterHalfEllipse.js
@@ -12,7 +12,6 @@ define( function( require ) {
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
   var Path = require( 'SCENERY/nodes/Path' );
-  var Node = require( 'SCENERY/nodes/Node' );
   var Shape = require( 'KITE/Shape' );
   var VisibleColor = require( 'SCENERY_PHET/VisibleColor' );
 
@@ -28,23 +27,20 @@ define( function( require ) {
    */
   function FilterHalfEllipse( filterWavelengthProperty, filterVisibleProperty, centerX, centerY, radiusX, radiusY, left ) {
 
-    Node.call( this );
-
     var shape = new Shape()
       .moveTo( centerX, centerY - radiusY )
       .ellipticalArc( centerX, centerY, radiusX, radiusY, 0, -Math.PI / 2, Math.PI / 2, left )
       .close();
 
-    var path = new Path( shape );
+    Path.call( this, shape );
+    var thisNode = this;
 
     filterWavelengthProperty.link( function( wavelength ) {
-      path.fill = VisibleColor.wavelengthToColor( wavelength );
+      thisNode.fill = VisibleColor.wavelengthToColor( wavelength );
     } );
 
     filterVisibleProperty.linkAttribute( this, 'visible' );
-
-    this.addChild( path );
   }
 
-  return inherit( Node, FilterHalfEllipse );
+  return inherit( Path, FilterHalfEllipse );
 } );
