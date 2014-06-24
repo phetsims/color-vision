a554156e5553d6468e61b65111492292380b4fd8
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-06-24 09:56:15 -0600

    move updateAnimationFrame and reset to inherit call, see #40

--- js/rgb/model/RGBPhotonBeam.js
+++ js/rgb/model/RGBPhotonBeam.js
@@ -34,65 +34,67 @@ define( function( require ) {
     this.frameCount = 0;
   }
 
-  var updateAnimationFrame = function( dt ) {
+  return inherit( PropertySet, RGBPhotonBeam, {
 
-    var intensity = this.intensityProperty.value;
+    updateAnimationFrame: function( dt ) {
 
-    // spacing keeps track of how many animations frames to skip between creating new photons when the intensity is low.
-    // this feature wasn't in the Java code, but I thought that creating a photon every frame looked too intense when the
-    // intensity was set as low as it could go.
-    var spacing = Math.floor( CYCLE_LENGTH / intensity );
+      var intensity = this.intensityProperty.value;
 
-    // create number of new photons proportional to intensity (between 1 and 5)
-    var numToCreate = ( intensity < CYCLE_LENGTH ) ? 1 : Math.floor( 0.05 * intensity );
+      // spacing keeps track of how many animations frames to skip between creating new photons when the intensity is low.
+      // this feature wasn't in the Java code, but I thought that creating a photon every frame looked too intense when the
+      // intensity was set as low as it could go.
+      var spacing = Math.floor( CYCLE_LENGTH / intensity );
 
-    // only create new photons if intensity is greater than 0
-    if ( intensity > 0 ) {
-      // if the intensity is low, don't create photons on every frame
-      if ( intensity >= CYCLE_LENGTH || this.frameCount % spacing === 0 ) {
+      // create number of new photons proportional to intensity (between 1 and 5)
+      var numToCreate = ( intensity < CYCLE_LENGTH ) ? 1 : Math.floor( 0.05 * intensity );
 
-        for ( var i = 0; i < numToCreate; i++ ) {
-          var newPhoton = RGBPhoton.createFromPool( this.beamLength, intensity );
+      // only create new photons if intensity is greater than 0
+      if ( intensity > 0 ) {
+        // if the intensity is low, don't create photons on every frame
+        if ( intensity >= CYCLE_LENGTH || this.frameCount % spacing === 0 ) {
 
-          // randomly offset the starting location of the photon
-          newPhoton.location.x += Math.random() * newPhoton.velocity.x * dt;
-          this.photons.push( newPhoton );
+          for ( var i = 0; i < numToCreate; i++ ) {
+            var newPhoton = RGBPhoton.createFromPool( this.beamLength, intensity );
+
+            // randomly offset the starting location of the photon
+            newPhoton.location.x += Math.random() * newPhoton.velocity.x * dt;
+            this.photons.push( newPhoton );
+          }
+        }
+
+        this.frameCount++;
+        if ( this.frameCount >= CYCLE_LENGTH ) {
+          this.frameCount = 0;
         }
       }
 
-      this.frameCount++;
-      if ( this.frameCount >= CYCLE_LENGTH ) {
-        this.frameCount = 0;
+      // move all photons that are currently active
+      for ( var j = 0; j < this.photons.length; j++ ) {
+
+        if ( this.photons[j].location.x > 0 && this.photons[j].location.y > 0 && this.photons[j].location.y < Constants.BEAM_HEIGHT ) {
+          this.photons[j].updateAnimationFrame( dt );
+
+        }
+        else {
+          this.perceivedIntensityProperty.value = this.photons[j].intensity;
+          this.photons[j].freeToPool();
+          this.photons.splice( j, 1 ); // remove jth RGBPhoton from list
+        }
+
+      }
+
+      // make sure that the intensity is 0 if there are no more photons
+      if ( this.photons.length === 0 ) {
+        this.perceivedIntensityProperty.value = 0;
+      }
+    },
+
+    reset: function() {
+      // set all photons to be out of bounds to trigger empty redraw
+      for ( var i = 0; i < this.photons.length; i++ ) {
+        this.photons[i].location.x = 0;
       }
     }
 
-    // move all photons that are currently active
-    for ( var j = 0; j < this.photons.length; j++ ) {
-
-      if ( this.photons[j].location.x > 0 && this.photons[j].location.y > 0 && this.photons[j].location.y < Constants.BEAM_HEIGHT ) {
-        this.photons[j].updateAnimationFrame( dt );
-
-      }
-      else {
-        this.perceivedIntensityProperty.value = this.photons[j].intensity;
-        this.photons[j].freeToPool();
-        this.photons.splice( j, 1 ); // remove jth RGBPhoton from list
-      }
-
-    }
-
-    // make sure that the intensity is 0 if there are no more photons
-    if ( this.photons.length === 0 ) {
-      this.perceivedIntensityProperty.value = 0;
-    }
-  };
-
-  var reset = function() {
-    // set all photons to be out of bounds to trigger empty redraw
-    for ( var i = 0; i < this.photons.length; i++ ) {
-      this.photons[i].location.x = 0;
-    }
-  };
-
-  return inherit( PropertySet, RGBPhotonBeam, { updateAnimationFrame: updateAnimationFrame, reset: reset } );
+  } );
 } );
--- js/singlebulb/model/SingleBulbPhotonBeam.js
+++ js/singlebulb/model/SingleBulbPhotonBeam.js
@@ -31,124 +31,125 @@ define( function( require ) {
     this.model = model;
   }
 
-  var updateAnimationFrame = function( dt ) {
+  return inherit( PropertySet, SingleBulbPhotonBeam, {
 
-    function randomColor() {
-      var r = Math.floor( Math.random() * 256 );
-      var g = Math.floor( Math.random() * 256 );
-      var b = Math.floor( Math.random() * 256 );
-      return new Color( r, g, b, 1 );
-    }
+    updateAnimationFrame: function( dt ) {
 
-    var halfWidth = SingleBulbConstants.GAUSSIAN_WIDTH / 2;
-    var probability = 1; // probability for a given photon to pass the filter
-
-    // if the flashlight is on, create new photons this animation frame
-    if ( this.model.flashlightOn ) {
-
-      // create a number of photon proportional to dt
-      // 5 photons will be created if dt is the expected 0.016
-      // if dt is greater, a greater number of photons will be created to compensate for the slower frame rate
-      var numToCreate = Math.random() * Math.floor( 5 * dt / 0.016 );
-      for ( var i = 0; i < numToCreate; i++ ) {
-        var newColor = ( this.model.light === 'white' ) ? randomColor() : VisibleColor.wavelengthToColor( this.model.flashlightWavelength );
-        var newPhoton = SingleBulbPhoton.createFromPool( this.beamLength + Math.random() * Constants.X_VELOCITY * dt, 1, newColor, ( this.model.light === 'white' ) );
-
-        // randomly offset the starting location of the photon
-        this.photons.push( newPhoton );
+      function randomColor() {
+        var r = Math.floor( Math.random() * 256 );
+        var g = Math.floor( Math.random() * 256 );
+        var b = Math.floor( Math.random() * 256 );
+        return new Color( r, g, b, 1 );
       }
-    }
 
-    // move all photons that are currently active
-    for ( var j = 0; j < this.photons.length; j++ ) {
-      var photon = this.photons[j];
+      var halfWidth = SingleBulbConstants.GAUSSIAN_WIDTH / 2;
+      var probability = 1; // probability for a given photon to pass the filter
 
-      // check if the photon just passed through the filter location
-      if ( this.model.filterVisible && photon.location.x < this.filterOffset && !photon.passedFilter ) {
+      // if the flashlight is on, create new photons this animation frame
+      if ( this.model.flashlightOn ) {
 
-        // If the flashlightWavelength is outside the transmission width, no photons pass.
-        if ( this.model.flashlightWavelength < this.model.filterWavelength - halfWidth || this.model.flashlightWavelength > this.model.filterWavelength + halfWidth ) {
-          probability = 0;
+        // create a number of photon proportional to dt
+        // 5 photons will be created if dt is the expected 0.016
+        // if dt is greater, a greater number of photons will be created to compensate for the slower frame rate
+        var numToCreate = Math.random() * Math.floor( 5 * dt / 0.016 );
+        for ( var i = 0; i < numToCreate; i++ ) {
+          var newColor = ( this.model.light === 'white' ) ? randomColor() : VisibleColor.wavelengthToColor( this.model.flashlightWavelength );
+          var newPhoton = SingleBulbPhoton.createFromPool( this.beamLength + Math.random() * Constants.X_VELOCITY * dt, 1, newColor, ( this.model.light === 'white' ) );
+
+          // randomly offset the starting location of the photon
+          this.photons.push( newPhoton );
         }
-        // flashlightWavelength is within the transmission width, pass a linear percentage.
+      }
+
+      // move all photons that are currently active
+      for ( var j = 0; j < this.photons.length; j++ ) {
+        var photon = this.photons[j];
+
+        // check if the photon just passed through the filter location
+        if ( this.model.filterVisible && photon.location.x < this.filterOffset && !photon.passedFilter ) {
+
+          // If the flashlightWavelength is outside the transmission width, no photons pass.
+          if ( this.model.flashlightWavelength < this.model.filterWavelength - halfWidth || this.model.flashlightWavelength > this.model.filterWavelength + halfWidth ) {
+            probability = 0;
+          }
+          // flashlightWavelength is within the transmission width, pass a linear percentage.
+          else {
+            probability = 1 - Math.abs( this.model.filterWavelength - this.model.flashlightWavelength ) / halfWidth;
+          }
+
+          // set the probability to be 0.3 for white photons
+          probability = ( !photon.wasWhite ) ? probability : 0.3;
+
+          // remove a percentage of photons from the beam
+          if ( Math.random() >= probability ) {
+            photon.freeToPool();
+            this.photons.splice( j, 1 ); // remove jth photon from list
+          }
+          // if the beam is white, make sure it is the color of the filter
+          else if ( photon.isWhite ) {
+            photon.color = VisibleColor.wavelengthToColor( this.model.filterWavelength );
+            photon.isWhite = false;
+          }
+          // if the photon is not white
+          else {
+
+            // set the photonIntensity to be the same as the percentage passing through the filter,
+            // for use when setting the perceived color when the photon hits the eye.
+            // make sure the intensity is at least 0.2, otherwise it looks too black in the view
+            photon.intensity = ( probability < 0.2 ) ? 0.2 : probability;
+          }
+        }
+
+        // keep track of photons which pass the filter
+        if ( photon.location.x < this.filterOffset ) {
+          photon.passedFilter = true;
+        }
+
+        // move the photon unless it goes out of bounds
+        // see related bounds checking code in the view file SingleBulbPhotonBeamNode.js,
+        // since this does not seem to completely keep them inside the canvas bounds (for unknown reasons)
+        if ( photon.location.x > 0 && photon.location.y > 0 && photon.location.y < Constants.BEAM_HEIGHT ) {
+          photon.updateAnimationFrame( dt );
+        }
+
+        // if the photon goes out of bounds, update the lastPhotonColor property, which is used in determining the perceived color
         else {
-          probability = 1 - Math.abs( this.model.filterWavelength - this.model.flashlightWavelength ) / halfWidth;
-        }
+          var newPerceivedColor = ( photon.isWhite ) ? Color.WHITE : photon.color;
 
-        // set the probability to be 0.3 for white photons
-        probability = ( !photon.wasWhite ) ? probability : 0.3;
+          // don't update the lastPhotonColor unless it is different than before, for performance reasons
+          // and don't bother to update the color if the view is on beam mode
+          if ( !this.model.lastPhotonColor.equals( newPerceivedColor ) && this.model.beam === 'photon' ) {
 
-        // remove a percentage of photons from the beam
-        if ( Math.random() >= probability ) {
+            // if the photon was white, the perceived color keeps full intensity even when it passes the filter,
+            // otherwise it takes the intensity of the photon, which may have been partially filtered
+            this.model.lastPhotonColor = ( photon.wasWhite ) ? newPerceivedColor.copy() : newPerceivedColor.withAlpha( photon.intensity );
+          }
           photon.freeToPool();
           this.photons.splice( j, 1 ); // remove jth photon from list
         }
-        // if the beam is white, make sure it is the color of the filter
-        else if ( photon.isWhite ) {
-          photon.color = VisibleColor.wavelengthToColor( this.model.filterWavelength );
-          photon.isWhite = false;
-        }
-        // if the photon is not white
-        else {
-
-          // set the photonIntensity to be the same as the percentage passing through the filter,
-          // for use when setting the perceived color when the photon hits the eye.
-          // make sure the intensity is at least 0.2, otherwise it looks too black in the view
-          photon.intensity = ( probability < 0.2 ) ? 0.2 : probability;
-        }
       }
 
-      // keep track of photons which pass the filter
-      if ( photon.location.x < this.filterOffset ) {
-        photon.passedFilter = true;
+      // emit a black photon for reseting the perceived color to black if no more photons passing through the filter.
+      // this takes care of the case when no photons pass through the filter
+      if ( probability === 0 && this.model.filterVisible && !this.model.perceivedColor.equals( Color.BLACK ) ) {
+        var blackPhoton = SingleBulbPhoton.createFromPool( this.filterOffset, 1, BLACK_ALPHA_0, false );
+        blackPhoton.passedFilter = true;
+        this.photons.push( blackPhoton );
       }
 
-      // move the photon unless it goes out of bounds
-      // see related bounds checking code in the view file SingleBulbPhotonBeamNode.js,
-      // since this does not seem to completely keep them inside the canvas bounds (for unknown reasons)
-      if ( photon.location.x > 0 && photon.location.y > 0 && photon.location.y < Constants.BEAM_HEIGHT ) {
-        photon.updateAnimationFrame( dt );
+      // if no photons exist, make sure the perceived color is black. This prevents the perceived color from
+      // getting stuck on when no photons are hitting the eye
+      if ( this.photons.length === 0 && !this.model.lastPhotonColor.equals( Color.BLACK ) ) {
+        this.model.lastPhotonColor = Color.BLACK;
       }
+    },
 
-      // if the photon goes out of bounds, update the lastPhotonColor property, which is used in determining the perceived color
-      else {
-        var newPerceivedColor = ( photon.isWhite ) ? Color.WHITE : photon.color;
-
-        // don't update the lastPhotonColor unless it is different than before, for performance reasons
-        // and don't bother to update the color if the view is on beam mode
-        if ( !this.model.lastPhotonColor.equals( newPerceivedColor ) && this.model.beam === 'photon' ) {
-
-          // if the photon was white, the perceived color keeps full intensity even when it passes the filter,
-          // otherwise it takes the intensity of the photon, which may have been partially filtered
-          this.model.lastPhotonColor = ( photon.wasWhite ) ? newPerceivedColor.copy() : newPerceivedColor.withAlpha( photon.intensity );
-        }
-        photon.freeToPool();
-        this.photons.splice( j, 1 ); // remove jth photon from list
+    reset: function() {
+      // set all photons to be out of bounds to trigger empty redraw
+      for ( var i = 0; i < this.photons.length; i++ ) {
+        this.photons[i].location.x = 0;
       }
     }
 
-    // emit a black photon for reseting the perceived color to black if no more photons passing through the filter.
-    // this takes care of the case when no photons pass through the filter
-    if ( probability === 0 && this.model.filterVisible && !this.model.perceivedColor.equals( Color.BLACK ) ) {
-      var blackPhoton = SingleBulbPhoton.createFromPool( this.filterOffset, 1, BLACK_ALPHA_0, false );
-      blackPhoton.passedFilter = true;
-      this.photons.push( blackPhoton );
-    }
-
-    // if no photons exist, make sure the perceived color is black. This prevents the perceived color from
-    // getting stuck on when no photons are hitting the eye
-    if ( this.photons.length === 0 && !this.model.lastPhotonColor.equals( Color.BLACK ) ) {
-      this.model.lastPhotonColor = Color.BLACK;
-    }
-  };
-
-  var reset = function() {
-
-    // set all photons to be out of bounds to trigger empty redraw
-    for ( var i = 0; i < this.photons.length; i++ ) {
-      this.photons[i].location.x = 0;
-    }
-  };
-
-  return inherit( PropertySet, SingleBulbPhotonBeam, { updateAnimationFrame: updateAnimationFrame, reset: reset } );
+  } );
 } );
