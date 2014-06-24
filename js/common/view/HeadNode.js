6a1bedc38261ad0d3ca28a180d7e43f56e2e0798
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-06-24 10:16:28 -0600

    add documentation to HeadNode, see #34

--- js/common/view/HeadNode.js
+++ js/common/view/HeadNode.js
@@ -12,16 +12,25 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Image = require( 'SCENERY/nodes/Image' );
 
+  // constants
+  var BOTTOM_OFFSET = 15;
+
   /**
    * @param {Image} image the image to display for the head
-   * @param {Number} bottom the bottom of the HeadNode (should be layoutBounds.bottom)
+   * @param {Number} layoutBoundsBottom should be layoutBounds.bottom so the HeadNode can align relative to that
    * @constructor
    */
-  function HeadNode( image, bottom ) {
+  function HeadNode( image, layoutBoundsBottom ) {
 
+    // HeadNode is just a wrapper around an image to ensure the correct positioning and scaling of all head images
+    // in color vision. It is useful because we want the heads to look identical on both screens, and because the
+    // RGB screen uses to overlapped head images to make sure the photons dissapear at the correct position,
+    // and because we want the brain view and no brain view to line up precisely.
     Image.call( this, image,
       {
-        bottom: bottom + 15,
+        // the head nodes all have the same bottom offset to ensure that the bottom of the head images align with
+        // the bottom of the screen in a typical browser.
+        bottom: layoutBoundsBottom + BOTTOM_OFFSET,
         left: 50,
         scale: 0.7
       } );
