491f253fd0cc53a36a3ef8995cbfa65abc2396dc
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-06-26 21:13:51 -0600

    add comments to constants file

--- js/ColorVisionConstants.js
+++ js/ColorVisionConstants.js
@@ -8,11 +8,11 @@ define( function( require ) {
   'use strict';
 
   return {
-    BEAM_HEIGHT: 130,
-    CENTER_Y_OFFSET: -20,
+    BEAM_HEIGHT: 130,     // height of all photonBeamNodes from both screens
+    CENTER_Y_OFFSET: -20, // many nodes are positioned with this offset from layoutBounds.centerY
+    X_VELOCITY: -240,     // x-velocity of photons, in screenview pixels / second
+    FAN_FACTOR: 1.05,     // amount of fanning of photons
     HOME_SCREEN_ICON_FILL: '#222222',
-    X_VELOCITY: -240,   // x-velocity of photons, in screenview pixels / second
-    FAN_FACTOR: 1.05,   // amount of fanning of photons
 
     // limit the max dt to be 0.05 to avoid gaps in the photon stream when a longer dt comes along.
     // most platforms should be getting more than 20 fps,
