8cd464737f67568d1837b1eaf5df742b8b2e3387
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-08-07 15:48:04 -0600

    change homescreen icon background, see #49

--- js/ColorVisionConstants.js
+++ js/ColorVisionConstants.js
@@ -12,7 +12,7 @@ define( function( require ) {
     CENTER_Y_OFFSET: -20, // many nodes are positioned with this offset from layoutBounds.centerY
     X_VELOCITY: -240,     // x-velocity of photons, in screenview pixels / second
     FAN_FACTOR: 1.05,     // amount of fanning of photons
-    HOME_SCREEN_ICON_FILL: '#222222',
+    HOME_SCREEN_ICON_FILL: 'rgb(180,180,180)',
 
     // limit the max dt to be 0.05 to avoid gaps in the photon stream when a longer dt comes along.
     // most platforms should be getting more than 20 fps,
