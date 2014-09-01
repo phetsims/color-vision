5c97d4554dafa4278e2f6009b6872955c464c220
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-09-01 12:42:02 -0600

    fix bug with turning RGB sliders on and off, see #75

--- js/rgb/view/RGBPhotonBeamNode.js
+++ js/rgb/view/RGBPhotonBeamNode.js
@@ -47,7 +47,10 @@ define( function( require ) {
 
       context.fillStyle = this.color;
       for ( var i = 0; i < this.photons.length; i++ ) {
-        context.fillRect( this.photons[i].location.x, this.photons[i].location.y, 3, 2 );
+        // don't draw photons with intensity 0, since these are just used for ensuring the perceived color is black
+        if ( this.photons[i].intensity !== 0 ) {
+          context.fillRect( this.photons[i].location.x, this.photons[i].location.y, 3, 2 );
+        }
       }
     },
 
