e4655877894ff0dffbb5ac7a037f665bbc7bad2b
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-02-06 08:27:27 -0700

    fix assertion error, see #86

--- js/singlebulb/view/FlashlightWireNode.js
+++ js/singlebulb/view/FlashlightWireNode.js
@@ -27,7 +27,7 @@ define( function( require ) {
       .lineTo( start.x + width - radius, start.y )
       .arc( start.x + width - radius, start.y - radius, radius, Math.PI / 2, 0, true )
       .lineTo( start.x + width, end.y + radius )
-      .arc( start.x + width - radius, end.y + radius, radius, 0, -Math.PI / 2, true )
+      .arc( start.x + width - radius, end.y + radius, radius, 0, 3 * Math.PI / 2, true )
       .lineTo( end.x, end.y );
 
     Path.call( this, wire,
