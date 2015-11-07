ba3962e59f79082bfd39c9d14c9007bdc19d3627
samreid <reids@colorado.edu>
2015-11-06 20:50:35 -0700

    updated copyright info using grunt update-copyright-dates, see  https://github.com/phetsims/chipper/issues/403

--- js/singlebulb/model/SingleBulbPhotonBeam.js
+++ js/singlebulb/model/SingleBulbPhotonBeam.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2014-2015, University of Colorado Boulder
 
 /**
  * Model of the photon beam used on the single bulb screen, made of individual photon particles.
--- js/singlebulb/view/FilterHalfEllipse.js
+++ js/singlebulb/view/FilterHalfEllipse.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2014, University of Colorado Boulder
 
 /**
  * FilterHalfEllipse is used to form the actual filter image. It is cut in half so
