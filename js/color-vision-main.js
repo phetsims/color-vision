e1be4caf28f6e681acc42d1a1a91ef5e7ea733dc
Andrew Adare <andrewadare@gmail.com>
2016-08-11 08:49:33 -0600

    https://github.com/phetsims/phet-io/issues/497 Remove require statement for API file

--- js/color-vision-main.js
+++ js/color-vision-main.js
@@ -15,9 +15,6 @@ define( function( require ) {
   var SimLauncher = require( 'JOIST/SimLauncher' );
   var Tandem = require( 'TANDEM/Tandem' );
 
-  // If running as phet-io, load the API
-  require( 'ifphetio!PHET_IO/simulations/color-vision/color-vision-api' );
-
   // strings
   var colorVisionTitleString = require( 'string!COLOR_VISION/color-vision.title' );
 
