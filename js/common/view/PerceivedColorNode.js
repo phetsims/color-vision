a9a8f84eb3230f7b4a9962a80d6ff65d63c9f99f
Chris Malley <cmalley@pixelzoom.com>
2018-05-11 11:23:16 -0600

    use Shape.newSubpath to draw 4 thought bubbles as one Shape
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/view/PerceivedColorNode.js
+++ js/common/view/PerceivedColorNode.js
@@ -11,7 +11,6 @@ define( function( require ) {
   // modules
   var colorVision = require( 'COLOR_VISION/colorVision' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
   var Shape = require( 'KITE/Shape' );
 
@@ -22,60 +21,28 @@ define( function( require ) {
    */
   function PerceivedColorNode( perceivedColorProperty, options ) {
 
-    options = options || {};
-
-    // Add bubbles, from largest to smallest. Bubbles are positioned relative to the center of the largest bubble.
-    assert && assert( !options.children, 'PerceivedColorNode sets children' );
-    options.children = [
-      new ThoughtBubble( perceivedColorProperty, 45, {
-        centerX: 0,
-        centerY: 0
-      } ),
-      new ThoughtBubble( perceivedColorProperty, 15, {
-        centerX: -130,
-        centerY: 45
-      } ),
-      new ThoughtBubble( perceivedColorProperty, 12, {
-        centerX: -158,
-        centerY: 105
-      } ),
-      new ThoughtBubble( perceivedColorProperty, 7, {
-        centerX: -170,
-        centerY: 160
-      } )
-    ];
-
-    Node.call( this, options );
-  }
-
-  colorVision.register( 'PerceivedColorNode', PerceivedColorNode );
-
-  inherit( Node, PerceivedColorNode );
-
-  /**
-   * One of the 'thought bubbles' in the perceived color display.
-   * @param {Property.<Color|string>} perceivedColorProperty
-   * @param {number} yRadius
-   * @param {Object} [options]
-   * @constructor
-   */
-  function ThoughtBubble( perceivedColorProperty, yRadius, options ) {
-
     options = _.extend( {
       lineWidth: 0.5,
       stroke: '#c0b9b9' // gray
     }, options );
 
-    var ellipse = new Shape().ellipse( 0, 0, 2 * yRadius, yRadius, 0 );
-    Path.call( this, ellipse, options );
+    // four thought bubbles, described from largest to smallest
+    var shape = new Shape()
+      .ellipse( 0, 0, 90, 45, 0 )
+      .newSubpath()
+      .ellipse( -130, 45, 30, 15, 0 )
+      .newSubpath()
+      .ellipse( -158, 105, 24, 12, 0 )
+      .newSubpath()
+      .ellipse( -170, 160, 14, 7, 0 );
+
+    Path.call( this, shape, options );
 
     perceivedColorProperty.linkAttribute( this, 'fill' );
   }
 
-  colorVision.register( 'PerceivedColorNode.ThoughtBubble', ThoughtBubble );
+  colorVision.register( 'PerceivedColorNode', PerceivedColorNode );
 
-  inherit( Path, ThoughtBubble );
-
-  return PerceivedColorNode;
+  return inherit( Path, PerceivedColorNode );
 } );
  
