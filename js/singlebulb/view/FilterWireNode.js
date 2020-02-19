2a51a071597521c3780b6342d5c92acf6526e06b
phet-dev <phetadmin@gmail.com>
2020-02-19 03:24:11 -0700

    update copyright dates from daily grunt work

--- js/singlebulb/view/FilterWireNode.js
+++ js/singlebulb/view/FilterWireNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * FilterWireNode forms the wire going from the filter to the slider, including the filter enabled switch.
