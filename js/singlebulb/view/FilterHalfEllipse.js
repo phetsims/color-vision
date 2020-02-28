6f9d6dd4930e3d076716164f38c9aee0247224c2
phet-dev <phetadmin@gmail.com>
2020-02-28 03:16:58 -0700

    update copyright dates from daily grunt work

--- js/singlebulb/view/FilterHalfEllipse.js
+++ js/singlebulb/view/FilterHalfEllipse.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * FilterHalfEllipse is used to form the actual filter image. It is cut in half so
