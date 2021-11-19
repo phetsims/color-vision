bcc851704828412d3cccc0febe02a190ca5f2160
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:02 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/colorVisionStrings.ts
@@ -0,0 +1,32 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import colorVision from './colorVision.js';
+
+type StringsType = {
+  'filterSlider': {
+    'label': string
+  },
+  'RgbBulbsModule': {
+    'title': string
+  },
+  'color-vision': {
+    'title': string
+  },
+  'bulbSlider': {
+    'label': string
+  },
+  'SingleBulbModule': {
+    'title': string
+  }
+};
+
+const colorVisionStrings = getStringModule( 'COLOR_VISION' ) as StringsType;
+
+colorVision.register( 'colorVisionStrings', colorVisionStrings );
+
+export default colorVisionStrings;
