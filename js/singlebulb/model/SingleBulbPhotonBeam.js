249f1a42c0f411ae62cce93bd227fb5d6e3fbd48
phet-dev <phetadmin@gmail.com>
2022-08-02 04:29:46 -0600

    update copyright dates from daily grunt work

--- js/singlebulb/model/SingleBulbPhotonBeam.js
+++ js/singlebulb/model/SingleBulbPhotonBeam.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * Model of the photon beam used on the single bulb screen, made of individual photon particles.
