4dbf7858101dacf27e98d1c8595cc711ce50783c
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2024-05-24 09:52:32 -0600

    Improve code readability. See https://github.com/phetsims/color-vision/issues/85.

--- js/common/view/FlashlightNode.js
+++ js/common/view/FlashlightNode.js
@@ -6,7 +6,6 @@
  * @author Luisa Vargas
  */
 
-import Vector2 from '../../../../dot/js/Vector2.js';
 import { Shape } from '../../../../kite/js/imports.js';
 import { Color, HBox, Line, LinearGradient, Node, Path, Rectangle } from '../../../../scenery/js/imports.js';
 import colorVision from '../../colorVision.js';
@@ -30,37 +29,46 @@ class FlashlightNode extends Node {
     const hSpaceFromBody = 10;
 
     // Define the colors for the flashlight gradient
-    const darkestColor = new Color( 138, 139, 140 );
-    const middleColor = new Color( 186, 187, 189 );
-    const lightestColor = new Color( 227, 228, 229 );
+    const flashlightColors = [
+      new Color( 138, 139, 140 ), // darkest color
+      new Color( 186, 187, 189 ), // middle color
+      new Color( 227, 228, 229 )  // lightest color
+    ];
+
+    // Define the flashlight stroke's gradient colors
+    const strokeColors = [
+      new Color( 68, 69, 70 ),     // darkest stroke color
+      new Color( 75, 76, 77 ),
+      new Color( 97, 98, 98 ),
+      new Color( 114, 114, 115 )   // lightest stroke color
+    ];
 
     // Calculate the x and y offsets for the gradient based on the rotation
     const xOffset = Math.sin( rotation ) * headHeight / 2;
     const yOffset = Math.cos( rotation ) * headHeight / 2;
 
-    // Function to create the flashlight gradient. The gradient is rotated based on the rotation of the flashlight,
-    // which affects the direction of the gradient to give the flashlight a realistic metallic look.
-    const createFlashlightGradient = () => {
-      return new LinearGradient( -xOffset, -yOffset, xOffset, yOffset )
-        .addColorStop( 0, darkestColor )
-        .addColorStop( 0.15, middleColor )
-        .addColorStop( 0.3, lightestColor )
-        .addColorStop( 0.45, lightestColor )
-        .addColorStop( 0.6, middleColor )
-        .addColorStop( 0.9, darkestColor );
+    // Function to creates a rotated LinearGradient for the flashlight using 'flashlightColors'.
+    // gradientHeightAdjustment is used for vertical shift to achieve an overall metallic look.
+    const createFlashlightGradient = gradientHeightAdjustment => {
+      return new LinearGradient( -xOffset, -yOffset + gradientHeightAdjustment, xOffset, yOffset + gradientHeightAdjustment )
+        .addColorStop( 0, flashlightColors[ 0 ] )
+        .addColorStop( 0.15, flashlightColors[ 1 ] )
+        .addColorStop( 0.3, flashlightColors[ 2 ] )
+        .addColorStop( 0.45, flashlightColors[ 2 ] )
+        .addColorStop( 0.6, flashlightColors[ 1 ] )
+        .addColorStop( 0.9, flashlightColors[ 0 ] );
     };
 
     // Define the flashlight gradient
-    const flashlightHeadFillGradient = createFlashlightGradient();
+    const flashlightHeadFillGradient = createFlashlightGradient( 0 );
 
     // Define the flashlight stroke's gradient
-    const darkestStrokeColor = new Color( 68, 69, 70 );
     const flashlightStrokeGradient = new LinearGradient( -xOffset, -yOffset, xOffset, yOffset )
-      .addColorStop( 0, darkestStrokeColor )
-      .addColorStop( 0.15, new Color( 97, 98, 98 ) )
-      .addColorStop( 0.3, new Color( 114, 114, 115 ) )
-      .addColorStop( 0.75, new Color( 75, 76, 77 ) )
-      .addColorStop( 1, darkestStrokeColor );
+      .addColorStop( 0, strokeColors[ 0 ] )
+      .addColorStop( 0.15, strokeColors[ 2 ] )
+      .addColorStop( 0.3, strokeColors[ 3 ] )
+      .addColorStop( 0.75, strokeColors[ 1 ] )
+      .addColorStop( 1, strokeColors[ 0 ] );
 
     // Create the flashlight head as a trapezoidal shape
     const flashlightHeadShape = new Shape()
@@ -79,10 +87,8 @@ class FlashlightNode extends Node {
     flashlightHeadPath.addChild( flashlightHeadStroke );
 
     // Draw the flashlight body
-    const flashlightBodyFillGradient = createFlashlightGradient();
-    const gradientAdjustment = rotation < 0 ? -25 : ( rotation > 0 ? 15 : 0 );
-    flashlightBodyFillGradient.start = new Vector2( -xOffset, -yOffset + gradientAdjustment );
-    flashlightBodyFillGradient.end = new Vector2( xOffset, yOffset + gradientAdjustment );
+    const gradientHeightAdjustment = rotation < 0 ? -25 : ( rotation > 0 ? 15 : 0 );
+    const flashlightBodyFillGradient = createFlashlightGradient( gradientHeightAdjustment );
     const flashlightBody = new Rectangle( 0, -headHeight / 2, bodyLength, headHeight,
       { fill: flashlightBodyFillGradient, stroke: flashlightStrokeGradient, lineWidth: 0.5, scale: 0.675 } );
 
@@ -92,8 +98,9 @@ class FlashlightNode extends Node {
 
     // Create the vertical lines in the body
     const spacing = headHeight / 10.5;
+    const numberOfLines = 10;
     const linesNode = new Node();
-    for ( let i = 0; i < 10; i++ ) {
+    for ( let i = 0; i < numberOfLines; i++ ) {
       const index = ( i === 0 || i === 4 || i === 8 ) ? 0 : 1;
       const y = ( -headHeight / 2 ) + 3 + i * spacing;
       const line = new Line( flashlightBody.left + hSpaceFromBody, y, bodyLength - hSpaceFromBody, y, {
@@ -104,7 +111,7 @@ class FlashlightNode extends Node {
     }
     flashlightBody.addChild( linesNode );
 
-    // Combine the head and body to create the flashlight using HBox
+    // Combine the head and body to create the flashlight
     const flashlight = new HBox( {
       children: [ flashlightHeadPath, flashlightBody ],
       spacing: 0,
