1ffa74cb88c2e3e5f113b0396e52f22f1f274abe
phet-dev <phetadmin@gmail.com>
2024-07-13 03:46:04 -0600

    update copyright dates from daily grunt work

--- js/common/view/ColorVisionScreenView.js
+++ js/common/view/ColorVisionScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2023, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * View components shared by both screens. Subclassed by SingleBulbScreenView and RGBScreenView.
